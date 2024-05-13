697d8067b1dc91f951cc25ed121d5be868c9a652
senorbeast <44683244+senorbeast@users.noreply.github.com>
2024-05-14 02:20:36 +0530

    Add free roam mode state

--- src/components/react-ridge.ts
+++ src/components/react-ridge.ts
@@ -2,21 +2,28 @@ import { newRidgeState } from "react-ridge-state";
 import { canvasJSONType } from "./ButtonPanel";
 
 // Init state
+
+// Animations
 export const animationPauseS = newRidgeState<boolean>(true);
 export const animationDurationS = newRidgeState<number>(1500);
 export const animationFrameS = newRidgeState<number>(1);
 export const animationRelativeProgressS = newRidgeState<number>(0);
 
+export const prevStartTime = newRidgeState<number>(0); // to fix
+
+// ---------------------------------------------------------------------
+
+// General state for frames
 export const currentFrameS = newRidgeState<number>(0);
-
-// not used currently (Using fOIds of invisible store)
-// need to update the savedJSON to use this foIDs
-export const fOIdsState = newRidgeState<string[]>([]);
-
+export const fOIdsState = newRidgeState<string[]>([]); // keep track of Frame Object across frames
 export const framesS = newRidgeState<canvasJSONType[]>([
     {
         version: "5.3.0",
         objects: [],
     },
 ]);
-export const prevStartTime = newRidgeState<number>(0);
+
+// ---------------------------------------------------------------------
+
+// Modes
+export const modeS = newRidgeState<"freeRoam" | "frames">("freeRoam");
